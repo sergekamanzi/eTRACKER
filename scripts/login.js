410a582b0392b0f568599b56f3afd2725c6d389e
serge.kmz <s.kamanzi@alustudent.com>
2024-08-01 12:24:56 +0200

    frontend

--- scripts/login.js
+++ scripts/login.js
@@ -1,30 +1,29 @@
 document.getElementById('loginForm').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Prevent the default form submission
-  
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
-    try {
-      const response = await fetch('http://e-tracker-backend-mysql-production.up.railway.app/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        alert('Logged in successfully');
-        // Redirect to dashboard upon successful login
-        window.location.href = 'dashboard.html';
-      } else {
-        alert(data.message || 'Login failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred. Please try again later.');
+  event.preventDefault(); // Prevent the default form submission
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const response = await fetch('https://e-tracker-backend-mysql-production.up.railway.app/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert('Logged in successfully');
+      // Redirect to dashboard upon successful login
+      window.location.href = 'dashboard.html';
+    } else {
+      alert(data.message || 'Login failed');
     }
-  });
-  
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred. Please try again later.');
+  }
+});
